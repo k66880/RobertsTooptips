773d633723f261fe900040e8a0f06adc7c2e8162
RobertWu <k66880@qq.com>
2026-01-18 14:19:19 +0800

    移除一些无用代码

--- src/main/java/com/robertsworks/robertstooltips/util/RGTHelper.java
+++ src/main/java/com/robertsworks/robertstooltips/util/RGTHelper.java
@@ -5,13 +5,9 @@
  */
 package com.robertsworks.robertstooltips.util;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 
 public class RGTHelper {
     /**
@@ -66,60 +62,5 @@ public class RGTHelper {
     public static boolean isUnbreakable(ItemStack stack) {
         if (!stack.isDamageableItem()) return true;
         return stack.getTag() != null && stack.getTag().getBoolean("Unbreakable");
-    }
-
-    /**
-     * 向全体玩家发送一个通知
-     * @param level 要通知的世界
-     * @param message 要通知的内容
-     * @return 返回是否通知成功
-     */
-    public static boolean notifyAllPlayers(Level level, Component message) {
-        try {
-            if (level instanceof ServerLevel serverLevel) {
-                MinecraftServer server = serverLevel.getServer();
-                server.getPlayerList().broadcastSystemMessage(message, false);
-                return true;
-            }
-        } catch (Exception e) {}
-        return false;
-    }
-
-    /**
-     * 设置游戏时间
-     * @param level 要设置的世界
-     * @param ticks 要设置的时间（tick数，20Tick = 1Second）
-     * @return
-     */
-    public static boolean setGameTime(Level level, long ticks) {
-        try {
-            if (level instanceof ServerLevel serverLevel) {
-                serverLevel.setDayTime(ticks);
-                return true;
-            }
-        } catch (Exception e) {}
-        return false;
-    }
-
-    /**
-     * 克隆一个ItemStack，堆叠数量与原ItemStack相同
-     * @param stack 要克隆的ItemStack
-     * @return
-     */
-    public static ItemStack clonItemStack(ItemStack stack) {
-        return clonItemStack(stack, stack.getCount());
-    }
-
-    /**
-     * 克隆一个ItemStack
-     * @param stack 要克隆的ItemStack
-     * @param count 新的ItemStack的堆叠数量
-     * @return
-     */
-    public static ItemStack clonItemStack(ItemStack stack, int count) {
-        if (stack.isEmpty()) return ItemStack.EMPTY;
-        var newStack = stack.copy();
-        newStack.setCount(count);
-        return newStack;
     }
 }
